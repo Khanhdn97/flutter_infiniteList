d8b8aa61aa57828ddc795fe3903a0170719de861
Khai Tran <tquangkhai98@gmail.com>
2023-06-13 13:29:49 +0700

    [U] - Remove warning

--- lib/comment_bloc.dart
+++ lib/comment_bloc.dart
@@ -1,24 +1,29 @@
-import 'dart:async';
+// ignore_for_file: invalid_use_of_visible_for_testing_member
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:infinite_list/comment_event.dart';
 import 'package:infinite_list/service.dart';
 import 'package:infinite_list/comment_state.dart';
 
-class CommentBloc extends Bloc<CommentEvent,CommentState> {
-  CommentBloc() : super(CommentState.intial()){
-    getCommentFromApi(0, 10).then((value){
+class CommentBloc extends Bloc<CommentEvent, CommentState> {
+  CommentBloc() : super(CommentState.intial()) {
+    getCommentFromApi(0, 10).then((value) {
       emit(state.add(value));
     });
   }
-  void handleActionBloc () async{
-      print(state.commentList.length);
-      final newLoad = await getCommentFromApi(state.commentList.length, 10).then((value) {
-        return value;
-      });
+  void handleActionBloc() async {
+    print(state.commentList.length);
+    final newLoad =
+        await getCommentFromApi(state.commentList.length, 10).then((value) {
+      return value;
+    });
+    if (kDebugMode) {
       print(newLoad);
-      emit(state.add(newLoad));
+    }
+    emit(state.add(newLoad));
+    if (kDebugMode) {
       print(state.commentList);
+    }
   }
 }
